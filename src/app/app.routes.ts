02057ca9afe34f79aa475670341cbad535c81514
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-06-26 20:23:44 -0300

    feat: create add component and lazy loading route to component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { AddComponent } from './features/add/add.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListComponent,
+  },
+  {
+    path: 'add-product',
+    loadComponent: () =>
+      import('./features/add/add.component').then((m) => m.AddComponent),
   },
 ];
