657a9a968bfeb7ac42331bd64591c6fa2012d7e0
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-06-26 21:22:17 -0300

    feat: create form to add component

--- src/app/features/add/add.component.ts
+++ src/app/features/add/add.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './add.component.html',
-  styleUrl: './add.component.scss'
+  styleUrl: './add.component.scss',
 })
 export class AddComponent {
+  formBuild = inject(FormBuilder);
 
+  addForm: FormGroup = this.formBuild.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    price: [0, [Validators.required]],
+  });
+
+  onSubmit() {
+    console.log(this.addForm.value);
+  }
 }
