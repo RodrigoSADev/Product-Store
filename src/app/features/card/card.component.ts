09a17dd36654d8b01222a7dd65091f7f9dc36024
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-06-26 22:14:40 -0300

    feat: add edit component

--- src/app/features/card/card.component.ts
+++ src/app/features/card/card.component.ts
@@ -1,7 +1,16 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../interfaces/product';
+import { MatDialog } from '@angular/material/dialog';
+import { EditComponent } from '../edit/edit.component';
 
 @Component({
   selector: 'app-card',
@@ -16,4 +25,12 @@ export class CardComponent {
   productTitle = computed(() => this.product().title);
   productDescription = computed(() => this.product().description);
   productPrice = computed(() => this.product().price);
+
+  modal = inject(MatDialog);
+  @Output() edit = new EventEmitter();
+
+  onEdit() {
+    this.edit.emit();
+    this.modal.open(EditComponent);
+  }
 }
